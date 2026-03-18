ecaa7c3170c442c23be101ba9b52725ae120f972
Eduardo Godoy <eduardogodoycelular@gmail.com>
2026-03-18 08:55:48 -0300

    início do projeto LineaLabs

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <title>Linea Labs</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container mt-5">
+        <h1 class="mb-4">Welcome to Linea Labs</h1>
+        <p class="lead">This is a simple landing page for Linea Labs.</p>
+        <a href="#" class="btn btn-primary">Learn More</a>
+    </div>
+</body>
+</html>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
